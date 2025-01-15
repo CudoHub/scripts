806fa82ef5ed287016e22b12fb6e5611bdd92861
CudoHub <139971521+CudoHub@users.noreply.github.com>
2025-01-15 05:19:30 +0300

    Update Ro-Ghoul Auto Farm.lua

--- Ro-Ghoul Auto Farm.lua
+++ Ro-Ghoul Auto Farm.lua
@@ -151,7 +151,7 @@ end
 
 tab2:AddSlider("TP Speed", function(x)
     myData.TeleportSpeed = x
-end, {min = 90, max = 250}):Set(45)
+end, {min = 90, max = 500}):Set(45)
 
 tab2:AddSlider("Distance from NPC", function(x)
     myData.DistanceFromNpc = x * -1
